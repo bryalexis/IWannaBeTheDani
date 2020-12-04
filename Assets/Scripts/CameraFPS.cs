b9b45f11344642ea757d3ec9765f3ce7506a1553
Bryan <bryan.ortiz@ug.uchile.cl>
2020-12-03 23:54:08 -0300

    Merge from BitBucket

---
+++ Assets/Scripts/CameraFPS.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class CameraFPS: MonoBehaviour {
+   //Desplazamiento en los ejes X y Z
+   float movX;
+   float movZ;
+   //rotación en el eje vertical (el FPC sólo va a rotar sobre este eje)
+   float rotY;
+   Vector3 mov;
+   //las velocidades de movimiento y rotación
+   public float vel = 8.0f;
+   public float velRot = 180.0f;
+ 
+   // Use this for initialization
+   void Start () {
+      Cursor.lockState = CursorLockMode.Locked;
+   }
+ 
+   // Update is called once per frame
+   void Update (){
+      rotY = Input.GetAxis("Mouse X") * velRot * Time.deltaTime;
+      transform.Rotate(0, rotY, 0);
+      movX = Input.GetAxisRaw ("Horizontal") * vel * Time.deltaTime;
+      movZ = Input.GetAxisRaw ("Vertical") * vel * Time.deltaTime;
+      mov = new Vector3 (movX, 0.0f, movZ);
+      transform.Translate (mov);
+   }
+}
